e5f3362b21973d5349ba6b0807a23c6d374afe3f
shalopai <bnyto@yandex.ru>
2024-07-06 01:01:26 +0700

    жопа

---
+++ src/main/java/org/goyda/authtotal/utils/HashManager.java
@@ -0,0 +1,20 @@
+package org.goyda.authtotal.utils;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+public class HashManager {
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes());
+            byte[] digest = md.digest();
+            StringBuilder sb = new StringBuilder();
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
