5e8ad39d700e64d4f663c702a98a433a33a3c15a
ivshostik <ivshostik@etu.ru>
2024-07-17 18:07:34 +0300

    rabochaya loshadka

--- src/main/java/org/goyda/authtotal/commands/utils/Messages.java
+++ src/main/java/org/goyda/authtotal/commands/utils/Messages.java
@@ -3,13 +3,15 @@ package org.goyda.authtotal.commands.utils;
 import org.bukkit.ChatColor;
 
 public class Messages {
-    public static final String REG_COMMAND = ChatColor.BOLD+""+ChatColor.GOLD+"T"+ ChatColor.WHITE+"M"+ChatColor.DARK_GRAY+" >"+ChatColor.RESET+""+ChatColor.GRAY+"Используйте кмоанду /register <пароль> <пароль>";
-    public static final String ALREADY_REGISTERED = ChatColor.BOLD+""+ChatColor.GOLD+"T"+ ChatColor.WHITE+"M"+ChatColor.DARK_GRAY+" >"+ChatColor.RESET+""+ChatColor.GRAY+" Вы уже зарегистрированы!";
-    public static final String REGISTERED_SUCCESSFULLY = ChatColor.BOLD+""+ChatColor.GOLD+"T"+ ChatColor.WHITE+"M"+ChatColor.DARK_GRAY+" >"+ChatColor.RESET+""+ChatColor.GRAY+"Вы успешно зарегистрированы!";
-    public static final String NOT_REGISTERED = ChatColor.BOLD+""+ChatColor.GOLD+"T"+ ChatColor.WHITE+"M"+ChatColor.DARK_GRAY+" >"+ChatColor.RESET+""+ChatColor.GRAY+"Вы не зарегистрированы!";
-    public static final String NOT_LOGIN = ChatColor.BOLD+""+ChatColor.GOLD+"T"+ ChatColor.WHITE+"M"+ChatColor.DARK_GRAY+" >"+ChatColor.RESET+""+ChatColor.GRAY+"Вы не вошли!";
-    public static final String ONLY_USERS = ChatColor.BOLD+""+ChatColor.GOLD+"T"+ ChatColor.WHITE+"M"+ChatColor.DARK_GRAY+" >"+ChatColor.RESET+""+ChatColor.GRAY+"Только игроки могут пользоваться этой командой!";
-    public static final String PASSWORDS_DOES_NOT_MATCH = ChatColor.BOLD+""+ChatColor.GOLD+"T"+ ChatColor.WHITE+"M"+ChatColor.DARK_GRAY+" >"+ChatColor.RESET+""+ChatColor.GRAY+"Пароли не совпадают!";
-    public static final String ALREADY_LOGINED = ChatColor.BOLD+""+ChatColor.GOLD+"T"+ ChatColor.WHITE+"M"+ChatColor.DARK_GRAY+" >"+ChatColor.RESET+""+ChatColor.GRAY+"Вы уже вошли!";
-    public static final String LOGIN_COMMAND = ChatColor.BOLD+""+ChatColor.GOLD+"T"+ ChatColor.WHITE+"M"+ChatColor.DARK_GRAY+" >"+ChatColor.RESET+""+ChatColor.GRAY+"Используйте команду /login <пароль>";
+    public static final String prefix =  ChatColor.BOLD + "" + ChatColor.GOLD + "T" + ChatColor.WHITE + "M"
+                                        + ChatColor.DARK_GRAY + " >" + ChatColor.RESET + "" + ChatColor.GRAY;
+    public static final String REG_COMMAND = prefix + "Используйте кмоанду /register <пароль> <пароль>";
+    public static final String ALREADY_REGISTERED = prefix + " Вы уже зарегистрированы!";
+    public static final String REGISTERED_SUCCESSFULLY = prefix + "Вы успешно зарегистрированы!";
+    public static final String NOT_REGISTERED = prefix + "Вы не зарегистрированы!";
+    public static final String NOT_LOGIN = prefix + "Вы не вошли!";
+    public static final String ONLY_USERS = prefix + "Только игроки могут пользоваться этой командой!";
+    public static final String PASSWORDS_DOES_NOT_MATCH = prefix + "Пароли не совпадают!";
+    public static final String ALREADY_LOGINED = prefix + "Вы уже вошли!";
+    public static final String LOGIN_COMMAND = prefix + "Используйте команду /login <пароль>";
 }
